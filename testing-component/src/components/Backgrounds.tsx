440770e9e096d4df9be9ce634e492b2aaa969b01
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-03-30 15:48:08 +0100

    Refactor Background component to support different file extensions

--- testing-component/src/components/Backgrounds.tsx
+++ testing-component/src/components/Backgrounds.tsx
@@ -24,21 +24,24 @@ type BackgroundType =
 interface BackgroundProps {
   className?: string;
   type: BackgroundType;
+  extension?: string;
 }
 
-const Background = forwardRef<HTMLImageElement, BackgroundProps>(({ className, type, ...rest }, ref) => {
-  return (
-    // <div>
-    <img
-      ref={ref}
-      aria-hidden="true"
-      src={`./${type}.png`}
-      alt=""
-      className={`m-auto absolute inset-[1px] w-full h-full pointer-events-none object-cover ${className}`}
-      {...rest}
-    />
-    // </div>
-  );
-});
+const Background = forwardRef<HTMLImageElement, BackgroundProps>(
+  ({ className, extension = "png", type, ...rest }, ref) => {
+    return (
+      // <div>
+      <img
+        ref={ref}
+        aria-hidden="true"
+        src={`./${type}.${extension}`}
+        alt=""
+        className={`m-auto absolute inset-[1px] w-full h-full pointer-events-none object-cover ${className}`}
+        {...rest}
+      />
+      // </div>
+    );
+  }
+);
 
 export default Background;
